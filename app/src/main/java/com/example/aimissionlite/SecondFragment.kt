03198ec13b7e8b81f36d9f7f92dae6cc6c1cf71a
Michael Widlok <michaelwidlok@Air-von-Michael.fritz.box>
2021-03-06 19:57:32 +0100

    removed wearable reference in manifest

--- app/src/main/java/com/example/aimissionlite/SecondFragment.kt
+++ app/src/main/java/com/example/aimissionlite/SecondFragment.kt
@@ -24,7 +24,6 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         view.findViewById<Button>(R.id.button_save_goal).setOnClickListener {
             val goalTitle = view.findViewById<EditText>(R.id.edit_text_title).text
             // call via viewmodel insert() and insert the new dataset into room. better is to transform this value to first fragment where we
