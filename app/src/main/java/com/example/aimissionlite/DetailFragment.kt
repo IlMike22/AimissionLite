e2865cc6dd4fef7e0d83894373c12a7d920fb54f
IlMike22 <michaelwidlok@gmail.com>
2021-04-17 11:09:06 +0200

    tried some live data and data-binding

--- app/src/main/java/com/example/aimissionlite/DetailFragment.kt
+++ app/src/main/java/com/example/aimissionlite/DetailFragment.kt
@@ -1,6 +1,8 @@
 package com.example.aimissionlite
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableStringBuilder
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +15,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.example.aimissionlite.DetailViewModel.DetailViewModelFactory
 import com.example.aimissionlite.models.domain.Genre
 import com.example.aimissionlite.models.domain.Goal
 import com.example.aimissionlite.models.domain.Priority
@@ -27,7 +31,10 @@ class DetailFragment : IDetailFragment, Fragment() {
     private var choosenPriority: Chip? = null
 
     private val viewModel: DetailViewModel by viewModels {
-        DetailViewModel.DetailViewModelFactory((this.activity?.application as AimissionApplication).repository)
+        DetailViewModelFactory(
+            resources,
+            (this.activity?.application as AimissionApplication).repository
+        )
     }
 
     override fun onCreateView(
@@ -47,15 +54,21 @@ class DetailFragment : IDetailFragment, Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Observer live data // one part of the 2 way binding
+        // in this case our vm can change a live data property and the layout will react
+//        val goalTitleObserver = Observer<String> { goalTitle ->
+//            var goalTitleEditText = view?.findViewById<EditText>(R.id.edit_text_title)
+//            //goalTitleEditText.text = SpannableStringBuilder(goalTitle)
+//        }
+//
+//        viewModel.goalTitle.observe(viewLifecycleOwner,goalTitleObserver)
+
         val chipGroupGenre =
             view.findViewById<ChipGroup>(R.id.chip_group_genre) // todo use data-binding
         val chipGroupPriority =
             view.findViewById<ChipGroup>(R.id.chip_group_priority) // todo use data-binding
 
-        chipGroupGenre.setOnCheckedChangeListener { group, checkedId ->
-            choosenGenre = view.findViewById(checkedId) // todo use data-binding
-            Toast.makeText(activity, "${choosenGenre?.text} clicked", Toast.LENGTH_SHORT).show()
-        }
+
 
         chipGroupPriority.setOnCheckedChangeListener { group, checkedId ->
             choosenPriority = view.findViewById(checkedId) // todo use data-binding
@@ -63,30 +76,30 @@ class DetailFragment : IDetailFragment, Fragment() {
                 .show()
         }
 
-        view.findViewById<Button>(R.id.button_save_goal).setOnClickListener {
-            val goalTitle = view.findViewById<EditText>(R.id.edit_text_title).text.toString()
-            val goalDescription =
-                view.findViewById<EditText>(R.id.edit_text_description).text.toString()
-
-            val currentDate = viewModel.getCurrentDate()
-
-            val newGoal = Goal(
-                id = 0,
-                title = goalTitle,
-                description = goalDescription,
-                creationDate = currentDate,
-                changeDate = currentDate,
-                isRepeated = false,
-                genre = choosenGenre.toGenre(),
-                status = Status.UNKOWN,
-                priority = choosenPriority.toPriority()
-            )
-            addGoal(newGoal)
-
-            val bundle =
-                bundleOf(resources.getString(R.string.bundle_argument_goal_title) to goalTitle)
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment, bundle)
-        }
+//        view.findViewById<Button>(R.id.button_save_goal).setOnClickListener {
+//            val goalTitle = view.findViewById<EditText>(R.id.edit_text_title).text.toString()
+//            val goalDescription =
+//                view.findViewById<EditText>(R.id.edit_text_description).text.toString()
+//
+//            val currentDate = viewModel.getCurrentDate()
+//
+//            val newGoal = Goal(
+//                id = 0,
+//                title = goalTitle,
+//                description = goalDescription,
+//                creationDate = currentDate,
+//                changeDate = currentDate,
+//                isRepeated = false,
+//                genre = choosenGenre.toGenre(),
+//                status = Status.UNKOWN,
+//                priority = choosenPriority.toPriority()
+//            )
+//            addGoal(newGoal)
+//
+//            val bundle =
+//                bundleOf(resources.getString(R.string.bundle_argument_goal_title) to goalTitle)
+//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment, bundle)
+//        }
     }
 
     override fun addGoal(goal: Goal) {
--- app/src/main/java/com/example/aimissionlite/DetailViewModel.kt
+++ app/src/main/java/com/example/aimissionlite/DetailViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.aimissionlite
 
+import android.content.res.Resources
 import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.*
 import com.example.aimissionlite.data.GoalRepository
 import com.example.aimissionlite.models.domain.Genre
@@ -8,25 +10,46 @@ import com.example.aimissionlite.models.domain.Goal
 import com.example.aimissionlite.models.domain.Priority
 import com.example.aimissionlite.models.domain.Status
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 import java.time.LocalDateTime
 
-class DetailViewModel(private val repository:GoalRepository): ViewModel() {
+class DetailViewModel(
+    private val resources: Resources,
+    private val repository: GoalRepository
+) : ViewModel() {
     val allGoals: LiveData<List<Goal>> = repository.allGoals.asLiveData()
-    val buttonText = "Hello VM Data Binding Button"
+    val buttonText = resources.getString(R.string.button_done)
+    val goalTitle:MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    fun setOnCheckedChangeListener(checkedId:Int) {
+        //choosenGenre = view.findViewById(checkedId) // todo use data-binding
+        println("!!! button clicked. This message comes from VM!!!")
+    }
+
+    fun onButtonClicked() {
+        println("!!! button clicked. This message comes from VM!!!")
+        println("!!! Goal title is ${goalTitle.value}")
+        goalTitle.value = "Now we change the goal title in our vm!"
+    }
 
     fun insert(goal: Goal) = viewModelScope.launch {
         repository.insert(goal)
     }
 
-    fun getCurrentDate():String = LocalDateTime.now().toString()
+    fun getCurrentDate(): String = LocalDateTime.now().toString()
 
-    class DetailViewModelFactory(private val repository:GoalRepository):ViewModelProvider.Factory {
+    class DetailViewModelFactory(
+        private val resources: Resources,
+        private val repository: GoalRepository
+    ) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return DetailViewModel(repository) as T
+                return DetailViewModel(resources, repository) as T
             }
             throw IllegalArgumentException("Unknown viewmodel class")
         }
